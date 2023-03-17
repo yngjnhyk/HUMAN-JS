fa724608f137306d43eeb15a77e52f4ddebf6f56
yngjnhyk <dskzpbmk@naver.com>
2023-03-17 14:50:11 +0900

    first commit

---
+++ 1_함수와 함수 호출의 차이.js
@@ -0,0 +1,15 @@
+const add = (a, b) => a + b;
+
+function calculator(func, a, b) {
+    return func(a, b);
+}
+
+
+add(3, 5);  // 8
+calculator(add, 3, 5);  // 8
+
+const onClick = () => (event) => {
+    console.log('hello');
+};
+
+document.querySelector('#header').addEventListener('click', onClick());
--- prac.js
+++
@@ -1,3 +0,0 @@
-const add = (a, b) => a + b;
-
-console.log(add(3,5));
